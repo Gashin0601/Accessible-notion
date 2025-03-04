3ef474bc8fa0ebb89858de147912cf3d3b255664
Gashin0601 <gashin6118@gmail.com>
2025-03-05 00:07:42 +0900

    初回コミット：Notion Accessibility Enhancer の実装

---
+++ notion-accessibility/background.js
@@ -0,0 +1,14 @@
+// 拡張機能のインストール時やアップデート時に実行
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Notion Accessibility Enhancer がインストールされました');
+});
+
+// タブが更新されたときの処理
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && tab.url.includes('notion.so')) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      files: ['content.js']
+    });
+  }
+}); 
